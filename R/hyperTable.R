b20b9983f4acc3a08d525954634ecf66478c26d6
DanStuder <100282876+DanStuder@users.noreply.github.com>
2023-03-09 11:31:21 +0100

    improved hyperTable

--- R/hyperTable.R
+++ R/hyperTable.R
@@ -1,40 +1,43 @@
 #' hyperTable
 #' 
-#' Berechnet eine Tabelle der hypergeometrisch verteilten Wahrscheinlichkeiten für x, für gegebene Randsummen.
+#' Berechnet eine Tabelle der hypergeometrisch verteilten Wahrscheinlichkeiten basierend auf n1p, np1 und n
 #'
-#' @param n11 
-#' @param n1p 
-#' @param n2p 
-#' @param np1 
-#' @param np2 
-#'
-#' @return
+#' @param n1p Grösse der Stichprobe 1
+#' @param np1 Anzahl Personen mit Merkmal 1
+#' @param n   Gesamte Stichprobengrösse
+#' @return Hyperparametrische Verteilungstabelle
+#' @importFrom stats dhyper fisher.test
 #' @export
-#'
 #' @examples
+#' hyperTable(6, 6, 13)
 
 
-filler = function(n11, n1p, n2p, np1, np2){
+hyperTable <- function(n1p, np1, n){
   
-  n12 = n1p - n11
-  n21 = np1 - n11
-  n22 = n2p - n21
+  # prevent output table from being in the scientific notation
+  options(scipen = 99)
   
-  return(matrix(c(n11, n12, n21, n22), nrow = 2, byrow = T))
-}
-
-
-
-selector <- function(n1p, n2p, np1, np2){
+  # Get the other margins
+  n2p = n - n1p
+  np2 = n - n2p
   
-  # Creat an empty object to be filled in the for-loop
+  # Define a function to fill up the rest of the matrix based on n11 and the margins
+  filler = function(n11, n1p, n2p, np1, np2){
+    
+    n12 = n1p - n11
+    n21 = np1 - n11
+    n22 = n2p - n21
+    
+    return(matrix(c(n11, n12, n21, n22), nrow = 2, byrow = T))
+  }
+  
+  # Create an empty object to be filled in the for-loop
   valid = NULL
-  
   
   # n11 can not be smaller than 0 or greater than n1p an np1, so that is where the loop starts and ends respectively.
   for(n11 in 0:min(n1p, np1)){
     
-    # gets the whole table from the margins and the element n11
+    # Pack Matrix
     fill = filler(n11, n1p, n2p, np1, np2) 
     
     # if any element of the table is lower than 0, the table is de facto not possible. 
@@ -47,77 +50,63 @@ selector <- function(n1p, n2p, np1, np2){
     } else {next}
   }
   
-  return(valid)
-  
-}
-
-
-
-n1p = 4
-n2p = 6
-np1 = 5
-np2 = 5
-
-hyperTable <- function(n1p, n2p, np1, np2){
-  
-  # prevent output table from being in the scientific notation
-  options(scipen = 99)
-  
-  # get the valid values for n11
-  valid = selector(n1p, n2p, np1, np2)
-  
-  # for every valid value of n11, calculate the left-sided cumulative probabilities
+  # Empty objects to be filled in the for-loop
   left = NULL
-  for(l in valid){
-    left <- append(left, 
-                   format(round(phyper(q = l, 
-                                       m = n1p, 
-                                       n = n2p, 
-                                       k = np2,
-                                       lower.tail = TRUE), 
-                                digits = 4), 
-                          nsmall = 4)
-    )
-  }
-  
-  # for every valid value of n11, calculate the point probabilities
   center = NULL
-  for(c in valid){
+  right = NULL
+  two = NULL
+  
+  
+  # for every valid value of n11, calculate the probabilities
+  for(v in valid){
+    
+    # create table
+    tab = filler(v,
+                 np1 = np1,
+                 np2 = np2,
+                 n1p = n1p,
+                 n2p = n2p)
+    
+    # Calculate point-probability
     center <- append(center, 
-                     format(round(dhyper(x = c, 
+                     format(round(dhyper(x = v, 
                                          m = n1p, 
                                          n = n2p, 
                                          k = np2), 
                                   digits = 4), 
-                            nsmall = 4)
-    )
-  }
-  
-  # for every valid value of n11, calculate the right-sided cumulative probabilities
-  right = NULL
-  for(r in rev(valid)){
+                            nsmall = 4))
+    
+    # Calculate left-sided probability
+    left <- append(left, 
+                   format(round(fisher.test(tab, 
+                                            alternative = "less")$p.value, 
+                                digits = 4),
+                          nsmall = 4))
+    
+    # Calculate right-sided probability
     right <- append(right, 
-                    format(round(phyper(q = r, 
-                                        m = n1p, 
-                                        n = n2p, 
-                                        k = np2,
-                                        lower.tail = TRUE), 
-                                 digits = 4), 
-                           nsmall = 4)
-    )
+                    format(round(fisher.test(tab, 
+                                             alternative = "greater")$p.value, 
+                                 digits = 4),
+                           nsmall = 4))
+    
+    # Calculate two-sided probability
+    two = append(two, 
+                 format(round(fisher.test(tab, 
+                                          alternative = "two.sided")$p.value, 
+                              digits = 4),
+                        nsmall = 4))
   }
   
-  
+
+  # Create output
   out = data.frame(valid,
                    left,
                    center,
-                   right)
+                   right,
+                   two)
   
-  colnames(out) = c("x", "X<=x", "X=x", "X>=x")
-  rownames(out) = NULL
+  colnames(out) = c("n11", "P(X<=x)", "P(X=x)", "P(X>=x)", "P'")
   
   return(out)
 }
-
-
-hyperTable(6,7,6,7)
